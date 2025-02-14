5f2d62a861904a2b814f18c9643f522eb24a1fba
joseDG <josethp1@gmail.com>
2025-02-13 22:48:27 -0500

    Agregando Matrices

---
+++ Modulo 16-Matrices/Matrices_Multidimensional/Program.cs
@@ -0,0 +1,68 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //Variables
+        byte i, j, numAlumnos, salones;
+        double sumaCalif = 0, promedio, califMin = 10, califMax = 0;
+
+        //Pedimos el número de salones
+        Console.Write("Ingrese el número de salones: ");
+        salones = Convert.ToByte(Console.ReadLine());
+
+        //Pedimos el número de alumnos
+        Console.Write("Ingrese el número de alumnos por salón: ");
+        numAlumnos = Convert.ToByte(Console.ReadLine());
+
+        //Creación de la matriz
+        double[,] calificaciones = new double[salones, numAlumnos];
+
+        //For para pedir calificaciones
+        for (i = 0; i < salones; i++)
+        {
+            Console.WriteLine("Salón {0}", i);
+            for (j = 0; j < numAlumnos; j++)
+            {
+                //Pedimos la califiación
+                Console.Write("Ingresa la calificación del alumno {0}: ", j);
+                calificaciones[i, j] = Convert.ToDouble(Console.ReadLine());
+
+                //Acumulamos las califiaciones
+                sumaCalif += calificaciones[i, j];
+            }
+        }
+
+        //Calculamos el promedio
+        promedio = sumaCalif / (salones * numAlumnos);
+
+        //Calculamos la calificación mínima
+        for (i = 0; i < salones; i++)
+        {
+            for (j = 0; j < numAlumnos; j++)
+            {
+                if (calificaciones[i, j] < califMin)
+                {
+                    califMin = calificaciones[i, j];
+                }
+            }
+        }
+
+        //Calculamos la califiación máxima
+        for (i = 0; i < salones; i++)
+        {
+            for (j = 0; j < numAlumnos; j++)
+            {
+
+                if (calificaciones[i, j] > califMax)
+                {
+                    califMax = calificaciones[i, j];
+                }
+            }
+        }
+
+        //Mostramos los resultados
+        Console.WriteLine("El promedio es: {0}", promedio);
+        Console.WriteLine("La califiación mínima es: {0}", califMin);
+        Console.WriteLine("La califiación máxima es: {0}", califMax);
+    }
+}
---
+++ Modulo 16-Matrices/Matrices_Unidimensionales/Program.cs
@@ -0,0 +1,53 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //Variables
+        byte i, numAlumnos;
+        double sumaCalif = 0, promedio, califMin = 10, califMax = 0;
+
+        //Pedimos el número de alumnos
+        Console.Write("Ingrese el número de alumnos: ");
+        numAlumnos = Convert.ToByte(Console.ReadLine());
+
+        //Creación de la matriz
+        double[] calificaciones = new double[numAlumnos];
+
+        //For para pedir calificaciones
+        for (i = 0; i < numAlumnos; i++)
+        {
+            //Pedimos la califiación
+            Console.Write("Ingresa la califiación: ");
+            calificaciones[i] = Convert.ToDouble(Console.ReadLine());
+
+            //Acumulamos las califiaciones
+            sumaCalif += calificaciones[i];
+        }
+
+        //Calculamos el promedio
+        promedio = sumaCalif / numAlumnos;
+
+        //Calculamos la calificación mínima
+        for (i = 0; i < numAlumnos; i++)
+        {
+            if (calificaciones[i] < califMin)
+            {
+                califMin = calificaciones[i];
+            }
+        }
+
+        //Calculamos la califiación máxima
+        for (i = 0; i < numAlumnos; i++)
+        {
+            if (calificaciones[i] > califMax)
+            {
+                califMax = calificaciones[i];
+            }
+        }
+
+        //Mostramos los resultados
+        Console.WriteLine("El promedio es: {0}", promedio);
+        Console.WriteLine("La califiación mínima es: {0}", califMin);
+        Console.WriteLine("La califiación máxima es: {0}", califMax);
+    }
+}
